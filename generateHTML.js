a5e89ba2ad405b27acc836e9dc746b68f64f824e
Serina Tsang <serinat@uw.edu>
2019-11-17 01:09:42 -0800

    updated js file

--- generateHTML.js
+++ generateHTML.js
@@ -29,193 +29,197 @@ module.exports.generateHTML = function generateHTML(data) {
     return `
     <!DOCTYPE html>
         <html lang="en">
-            <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-                <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>${data.name}'s Profile</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-           background-color: ${colors[data.color].wrapperBackground};
-           padding-top: 100px;
-           }
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'Cabin', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'BioRhyme', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid ${colors[data.color].photoBorderColor};
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-           }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
-           .container {
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
-  
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-  
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
-             margin: 20px;
-           }
-           
-           .col {
-           flex: 1;
-           text-align: center;
-           }
-  
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-  
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-        </style>
-      </head>
-
-      <body>
-        <div class="wrapper">
-        <img src="${data.profileImg}" alt="Profile Image">
-        <h1>Hi!</h1>
-        <h2>My name is ${data.actualName}!</h2>
-        <h4 class="company">Currently @ ${data.company}</h4>
-        <ul class="nav-links">
-            <li class="nav-link"><i class="fa fa-location-arrow">${data.location}</i></li>
-            <li class="nav-link"><i class="fa fa-github"><a href="${data.github}"></a>GitHub</i></li>
-            <li class="nav-link"><i class="fa fa-rss"><a href="${data.blog}"></a>Blog</i></li>
-        </ul>
-
-    <main>
-        <div class="container">
-            <div class="row">
-                <h3 class="col">${data.bio}</h3>
+           <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+              <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+              <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+              <title>${data.name}'s Profile</title>
+              <style>
+                  @page {
+                    margin: 0;
+                  }
+                 *,
+                 *::after,
+                 *::before {
+                 box-sizing: border-box;
+                 }
+                 html, body {
+                 padding: 0;
+                 margin: 0;
+                 }
+                 html, body, .wrapper {
+                 height: 100%;
+                 }
+                 .wrapper {
+                 background-color: ${colors[data.color].wrapperBackground};
+                 padding-top: 100px;
+                 }
+                 body {
+                 background-color: white;
+                 -webkit-print-color-adjust: exact !important;
+                 font-family: 'Cabin', sans-serif;
+                 }
+                 main {
+                 background-color: #E9EDEE;
+                 height: auto;
+                 padding-top: 30px;
+                 }
+                 h1, h2, h3, h4, h5, h6 {
+                 font-family: 'BioRhyme', serif;
+                 margin: 0;
+                 }
+                 h1 {
+                 font-size: 3em;
+                 }
+                 h2 {
+                 font-size: 2.5em;
+                 }
+                 h3 {
+                 font-size: 2em;
+                 }
+                 h4 {
+                 font-size: 1.5em;
+                 }
+                 h5 {
+                 font-size: 1.3em;
+                 }
+                 h6 {
+                 font-size: 1.2em;
+                 }
+                 .photo-header {
+                 position: relative;
+                 margin: 0 auto;
+                 margin-bottom: -50px;
+                 display: flex;
+                 justify-content: center;
+                 flex-wrap: wrap;
+                 background-color: ${colors[data.color].headerBackground};
+                 color: ${colors[data.color].headerColor};
+                 padding: 10px;
+                 width: 95%;
+                 border-radius: 6px;
+                 }
+                 .photo-header img {
+                 width: 250px;
+                 height: 250px;
+                 border-radius: 50%;
+                 object-fit: cover;
+                 margin-top: -75px;
+                 border: 6px solid ${colors[data.color].photoBorderColor};
+                 box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+                 }
+                 .photo-header h1, .photo-header h2 {
+                 width: 100%;
+                 text-align: center;
+                 }
+                 .photo-header h1 {
+                 margin-top: 10px;
+                 }
+                 .links-nav {
+                 width: 100%;
+                 text-align: center;
+                 padding: 20px 0;
+                 font-size: 1.1em;
+                 }
+                 .nav-link {
+                 display: inline-block;
+                 margin: 5px 10px;
+                 }
+                 .workExp-date {
+                 font-style: italic;
+                 font-size: .7em;
+                 text-align: right;
+                 margin-top: 10px;
+                 }
+                 .container {
+                 padding: 50px;
+                 padding-left: 100px;
+                 padding-right: 100px;
+                 }
+        
+                 .row {
+                   display: flex;
+                   flex-wrap: wrap;
+                   justify-content: space-between;
+                   margin-top: 20px;
+                   margin-bottom: 20px;
+                 }
+        
+                 .card {
+                   padding: 20px;
+                   border-radius: 6px;
+                   background-color: ${colors[data.color].headerBackground};
+                   color: ${colors[data.color].headerColor};
+                   margin: 20px;
+                 }
+                 
+                 .col {
+                 flex: 1;
+                 text-align: center;
+                 }
+        
+                 a, a:hover {
+                 text-decoration: none;
+                 color: inherit;
+                 font-weight: bold;
+                 }
+        
+                 @media print { 
+                  body { 
+                    zoom: .75; 
+                  } 
+                 }
+              </style>
+          </head>
+          <body>
+          <div class="wrapper">
+              <div class="photo-header" style="text-align: center">
+                  <img src="${data.profileImg}" alt="Profile Image">
+                  <h1>Hi!</h1>
+                  <h2>My name is ${data.actualName}!</h2>
+                  <h4 style="text-align: center">Currently @ ${data.company}</h4>
+                  <ul class="links-nav">
+                      <li class="nav-link"><i class="fa fa-location-arrow"> ${data.location}</i></li>
+                      <li class="nav-link"><a href="${data.github}"><i class="fa fa-github"> GitHub</i></a></li>
+                      <li class="nav-link"><a href="${data.blog}"><i class="fa fa-rss"> Blog</i></a></li>
+                  </ul>
+              </div>
+      
+              <main>
+                  <div class="container">
+                      <div class="row">
+                          <h4 class="col">${data.bio}</h4>
+                      </div>
+                      <div class="row">
+                          <div class="col">
+                              <div class="card">
+                                  <h3>Public Repositories</h3>
+                                  <h4>${data.publicRepos}</h4>
+                              </div>
+      
+                              <div class="card">
+                                  <h3>Followers</h3>
+                                  <h4>${data.followers}</h4>
+                              </div>
+                          </div>
+      
+                          <div class="col" style="flex: 1">
+                              <div class="card">
+                                  <h3>GitHub Stars</h3>
+                                  <h4>${data.stars}</h4>
+                              </div>
+      
+                              <div class="card">
+                                  <h3>Following</h3>
+                                  <h4>${data.following}</h4>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+               </main>
             </div>
-            <div class="row">
-            <div class="col">
-                <div class="card">
-                    <h3>Public Repositories</h3>
-                    <h4>${data.publicRepos}</h4>
-                </div>
-                <div class="card">
-                    <h3>Followers</h3>
-                    <h4>${data.followers}</h4>
-                </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <h3>GitHub Stars</h3>
-                        <h4>${data.stars}</h4>
-                    </div>
-                    <div class="card">
-                        <h3>Following</h3>
-                        <h4>${data.following}</h4>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-       </div> 
-    </body>
-</html>'  
-};  
+      </body>
+  </html>`
+  };
