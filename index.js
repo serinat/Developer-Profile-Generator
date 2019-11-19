c6acf708cb71923eba8cdec213b47c0b65326c70
Serina Tsang <serinat@uw.edu>
2019-11-19 01:21:57 -0800

    testing

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function promptUser() {
                     bio: user.bio,
                     publicRepos: user.public_repos,
                     followers: user.followers,
-                    stars: user.private_gists,
+                    stars: user.public_gists,
                     following: user.following,
                     color: color
                 }
